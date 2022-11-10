75e61888271525a5ae5eb0b36127b42a76034037
Celine Robinson <csr33@duke.edu>
2022-11-10 14:59:34 -0500

    usgs tnm

--- object_detection/volume_estimation/usgs_tnm_api.py
+++ object_detection/volume_estimation/usgs_tnm_api.py
@@ -15,7 +15,7 @@ def get_args_parse():
     parser = argparse.ArgumentParser(description='This script adds LPC data to tile level tank data')
     parser.add_argument('--tnm_url', type=str, default='https://tnmaccess.nationalmap.gov/api/v1/products',
                         help='tnm api url')
-    parser.add_argument('--tile_level_annotations_path', type=str,
+    parser.add_argument('--tank_data_path', type=str,
                         default="/hpc/group/borsuklab/csr33/volume_estimation/tile_level_annotations/tile_level_annotations.geojson",
                         help='path to hold, lidar by tank geojson data')
     parser.add_argument('--dataset_name', type=str, default=None,
@@ -35,8 +35,8 @@ def get_args_parse():
     return args
 
 def main(args):
-    tile_level_annotations = gpd.read_file(args.tile_level_annotations_path)
-    complete_df = vol_est.usgs_api(tile_level_annotations, args.tnm_url, args.dataset_name, args.request_total_idx, 
+    tank_data = gpd.read_file(args.tank_data_path)
+    complete_df = vol_est.usgs_api(tank_data, args.tnm_url, args.dataset_name, args.request_total_idx,
                                        args.request_content_idx, args.request_content_names_idx)
 
     complete_df.to_file(os.path.join(args.stored_data_path, args.dataset_abbrv + "_subset.geojson"), driver="GeoJSON")
