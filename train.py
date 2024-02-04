cedaa167dc417997d4ac125c346787ceaddce136
Celine Robinson <csr33@dcc-rekerlab-gpu-01.rc.duke.edu>
2024-02-03 22:17:50 -0500

    creating prediction pipeline

--- train.py
+++ train.py
@@ -5,28 +5,23 @@ import argparse
 def get_args_parse():
     parser = argparse.ArgumentParser("Exploratory Data Analysis")
     parser.add_argument("--data", default="/hpc/home/csr33/ast_object_detection/ast.yaml", type=str)
-    parser.add_argument("--model", default='yolov8n.pt', type=str)
-    parser.add_argument("--batch", default=8, type=int)
+    parser.add_argument("--model", default='yolov8x.pt', type=str)
     parser.add_argument("--imgsz", default=640, type=int)
-    parser.add_argument("--epochs", default=10, type=int)
+    parser.add_argument("--epochs", default=300, type=int)
     args = parser.parse_args()
     return args
 
 def train(args):
     comet_ml.init(project_name="yolov8-ast")
 
+    if args.name == None:
+        args.name = f'{os.path.splitext(args.model)[0]}_e{args.epochs}_imgsz{args.imgsz}'
     # Load the model.
     # Load a model
     model = YOLO(args.model)  # load a pretrained model (recommended for training)
-
     # Training.
-    results = model.train(
-       data=args.data,
-       imgsz=args.imgsz,
-       epochs=args.epochs,
-       batch=args.batch,
-       name=f'{os.path.splitext(args.model)[0]}_e{args.epochs}_b{args.batch}_imgsz{args.imgsz}')
-
+    results = model.train(project = args.project_name, name = args.name,
+                          imgsz=args.imgsz)
 if __name__ == '__main__':
     # Get the arguments
     args = get_args_parse()
---
+++ val.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=scavenger-gpu
+#SBATCH --mem=25GB # Memory per node. 5 GB
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1 # Number of GPUs per node
+#SBATCH --cpus-per-task=16
+#SBATCH --output /hpc/home/csr33/ast_object_detection/train_imgsz_512.out
+#SBATCH --error /hpc/home/csr33/ast_object_detection/train_imgsz_512.err
+
+module unload Anaconda3/2021.05
+source /hpc/home/csr33/miniconda3/etc/profile.d/conda.sh
+conda activate /hpc/group/borsuklab/cred/.conda/envs/yolov8
+
+#export COMET_API_KEY=CwpWEkJDRJc0rY57WYgoDuJvp
+
+# Update settings
+# https://docs.ultralytics.com/quickstart/
+                 
+# Update a setting
+yolo settings runs_dir="/work/csr33/object_detection/runs" weights_dir="/work/csr33/object_detection/weights"
+yolo detect val data="/hpc/home/csr33/ast_object_detection/ast.yaml" model="/work/csr33/object_detection/runs/detect/baseline_train/weights/best.pt"
